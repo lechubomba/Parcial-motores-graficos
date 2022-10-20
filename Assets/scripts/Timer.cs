6dced40751e168097f50055e4a74bc57b881fb64
franco-gabriel <lafuentefrancogabriel@gmail.com>
2022-10-20 04:30:54 -0300

    commit9

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -1,31 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 public class Timer : MonoBehaviour
 {
-	//Maximum time to complete level (in seconds)
-	public float MaxTime = 60f;
+    float currentTime;
+    public float startingTime = 60f; 
 
-	[SerializeField] private float CountDown = 0;
+    [SerializeField] TextMeshProUGUI countdownText;
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+    void Update()
+    {
+        currentTime = currentTime - 1 * Time.deltaTime;  
+        countdownText.text = currentTime.ToString("0");
 
-	void Start()
-	{
-		CountDown = MaxTime;
-	}
-	void update()
-	{
-		//Reduce time
-		CountDown -= Time.deltaTime;
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
 
-	}
-
-	
-
-
-
-
-
-
-
+          
+            SceneManager.LoadScene(0);
+        }
+    }
 }
